0b016ddc30c962fc6c1e827087e9577c1c92e7e0
Your Name <you@example.com>
2021-03-02 13:37:09 +0800

    修改部分变量名称

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,23 +46,23 @@ cat > main.py <<EOF
 import os
 from openstack import connection
 
-projectId = os.getenv('CDN_PROJECTID')
-cloud = os.getenv('CDN_CLOUD')
-region = os.getenv('CDN_REGION')
-AK = os.getenv('CDN_AK')
-SK = os.getenv('CDN_SK')
-refresh_urls = os.getenv('CDN_REFRESH_URLS')
-refresh_type = os.getenv('CDN_REFRESH_TYPE')
+cdn_projectId = os.getenv('CDN_PROJECTID')
+cdn_cloud = os.getenv('CDN_CLOUD')
+cdn_region = os.getenv('CDN_REGION')
+cdn_ak = os.getenv('CDN_AK')
+cdn_sk = os.getenv('CDN_SK')
+cdn_refresh_urls = os.getenv('CDN_REFRESH_URLS')
+cdn_refresh_type = os.getenv('CDN_REFRESH_TYPE')
 cdn_endpoint = os.getenv('CDN_ENDPOINT')
 
 os.environ.setdefault('OS_CDN_ENDPOINT_OVERRIDE', cdn_endpoint)
 
 conn = connection.Connection(
-    project_id=projectId,
-    cloud=cloud,
-    region=region,
-    ak=AK,
-    sk=SK)
+    project_id=cdn_projectId,
+    cloud=cdn_cloud,
+    region=cdn_region,
+    ak=cdn_ak,
+    sk=cdn_sk)
 
 def refresh_create(_refresh_task):
     task = conn.cdn.create_refresh_task(**_refresh_task)
@@ -71,8 +71,8 @@ def refresh_create(_refresh_task):
 
 if __name__ == "__main__":
     refresh_dir_task = {
-        "type": refresh_type,
-        "urls": [refresh_urls]
+        "type": cdn_refresh_type,
+        "urls": [cdn_refresh_urls]
     }
     print(refresh_dir_task)
     refresh_create(refresh_dir_task)
